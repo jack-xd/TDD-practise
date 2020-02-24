13f677505770e2ccd53c9077cbbbaed2ba1b9a5f
jacck-xd <jack_xiaodao@126.com>
2020-02-24 15:56:45 +0800

    0224打卡，先写todo清单，用时5分钟；然后根据清单编码，用时5分44秒。 1、对比2分钟内的视频，自己代码太长，需要精简，才有可能突破5分钟； 2、idea常用的快捷键熟悉，编码未用鼠标。下一步熟悉模版

--- FizzBuzz/src/test/java/fizzBuzz/GameTest.java
+++ FizzBuzz/src/test/java/fizzBuzz/GameTest.java
@@ -6,26 +6,26 @@ import org.junit.jupiter.api.Test;
 public class GameTest {
     @Test
     void should_print_normal_given_normal_number() {
-        assertEquals("1", 1);
-        assertEquals("2", 2);
+        testEquals("1", 1);
+        testEquals("2", 2);
     }
 
     @Test
     void should_print_fizz_given_number_divided_by_3() {
-        assertEquals("fizz",3);
+        testEquals("fizz",3);
     }
 
     @Test
     void should_print_buzz_given_number_divided_by_5() {
-        assertEquals("buzz",5);
+        testEquals("buzz",5);
     }
 
     @Test
-    void should_print_fizzbuzz_given_number_divided_by_15() {
-        assertEquals("fizzbuzz",15);
+    void should_print_fizzbuzz_given_number_divided_by15() {
+        testEquals("fizzbuzz",15);
     }
 
-    private void assertEquals( String s, int i ) {
+    private void testEquals( String s, int i ) {
         Assertions.assertEquals(s, new Game(i).toString());
     }
 }
