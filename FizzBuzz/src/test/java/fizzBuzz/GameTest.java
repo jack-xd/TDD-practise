0b580aabf3da4f10128500f6c3efdbb8ad59d6da
jacck-xd <jack_xiaodao@126.com>
2020-02-25 21:27:41 +0800

    0225打卡：今天用Java练习了fizzbuzz，用时4分钟56秒。终于突破了5分钟关口。体会：1、按计划学习了live template；2、在idea上设置了自己的第一个 live template；3、完成昨天突破5分钟的目标。感觉挺不错。

--- FizzBuzz/src/test/java/fizzBuzz/GameTest.java
+++ FizzBuzz/src/test/java/fizzBuzz/GameTest.java
@@ -1,31 +1,27 @@
 package fizzBuzz;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class GameTest {
     @Test
-    void should_print_normal_given_normal_number() {
-        testEquals("1", 1);
-        testEquals("2", 2);
+    void should_print_number_given_normal_number() {
+        assertEquals("1", new Game(1).toString() );
     }
 
     @Test
     void should_print_fizz_given_number_divided_by_3() {
-        testEquals("fizz",3);
+        assertEquals("fizz", new Game(3).toString() );
     }
 
     @Test
     void should_print_buzz_given_number_divided_by_5() {
-        testEquals("buzz",5);
+        assertEquals("buzz", new Game(5).toString() );
     }
 
     @Test
-    void should_print_fizzbuzz_given_number_divided_by15() {
-        testEquals("fizzbuzz",15);
-    }
-
-    private void testEquals( String s, int i ) {
-        Assertions.assertEquals(s, new Game(i).toString());
+    void should_print_fizzbuzz_given_number_divided_by_15() {
+        assertEquals("fizzbuzz", new Game(15).toString() );
     }
 }
