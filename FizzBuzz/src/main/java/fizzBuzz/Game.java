25fd967a8c82f800d14ec0676d4b92b3c3b6ae05
jacck-xd <jack_xiaodao@126.com>
2020-02-26 10:55:34 +0800

    reset

--- FizzBuzz/src/main/java/fizzBuzz/Game.java
+++ FizzBuzz/src/main/java/fizzBuzz/Game.java
@@ -9,9 +9,9 @@ public class Game {
 
     @Override
     public String toString() {
-        if(index%15==0) return "fizzbuzz";
-        if(index%3==0) return "fizz";
-        if(index%5==0) return "buzz";
+        if(index%15 == 0) return "fizzbuzz";
+        if(index%3 == 0) return "fizz";
+        if(index%5 == 0) return "buzz";
         return String.valueOf(index);
     }
 }
